ec00488e1f7727fdc25c7fbc1e3c1dd1db714b89
Sơn Nguyễn <son@sonng.dev>
2022-12-05 11:46:42 +0000

    feat: Remove logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, KeyboardEventHandler } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -68,14 +67,6 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <div
         ref={divRef}
         className="dynamic-text"
